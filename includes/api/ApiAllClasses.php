87690bb81a742a699f2c1ae935242662de3c8a09
soraia653 <soraiaoliveira094@gmail.com>
2022-11-04 19:51:36 +0100

    added more api endpoints

--- includes/api/ApiAllClasses.php
+++ includes/api/ApiAllClasses.php
@@ -11,21 +11,19 @@ class ApiAllClasses extends \ApiQueryBase {
 	 * @param string $moduleName
 	 */
 	public function __construct( $query, $moduleName ) {
-		parent::__construct( $query, $moduleName, 'ex' );
+		parent::__construct( $query, $moduleName );
 	}
 
 	public function execute() {
-		
+	
 		$db = $this->getDB();
 
 		$this->addTables('wm_classes');
+		
 		$this->addFields([
 			'class_id',
 			'class_name',
-			'class_start',
-			'class_end',
-			'class_teacher_id',
-			'class_token'
+			'class_teacher_user_id'
 		]);
 
 		$res = $this->select( __METHOD__ );
@@ -33,12 +31,9 @@ class ApiAllClasses extends \ApiQueryBase {
 
 		foreach($res as $row) {
 			$vals = [
-					'class-id' => (int)$row->class_id,
-					'class-name' => $row->class_name,
-					'class-start' => $row->class_start,
-					'class-end' => $row->class_end,
-					'class-teacher-id' => (int)$row->class_teacher_id,
-					'class-token' => $row->class_token
+					'id' => (int)$row->class_id,
+					'name' => $row->class_name,
+					'teacherid' => (int)$row->class_teacher_user_id
 				];
 			$fit = $result->addValue( [ 'query', $this->getModuleName() ], null, $vals );
 		}
@@ -48,9 +43,7 @@ class ApiAllClasses extends \ApiQueryBase {
 	protected function getExamplesMessages() {
 		return [
 			'action=query&list=allclasses'
-				=> 'apihelp-query+example-example-1',
-			'action=query&list=example&key=teacher_id'
-				=> 'apihelp-query+example-example-2',
+				=> 'apihelp-allclasses-example-1'
 		];
 	}
 }
--- includes/api/ApiCreateClass.php
+++ includes/api/ApiCreateClass.php
@@ -3,6 +3,7 @@
 namespace MediaWiki\Extension\Wikimini\api;
 
 use ApiBase;
+use User;
 use MediaWiki\MediaWikiServices;
 
 class ApiCreateClass extends ApiBase {
@@ -14,12 +15,15 @@ class ApiCreateClass extends ApiBase {
 	public function isWriteMode() {
         return true;
     }
+
+    public function MustBePosted() {
+        return true;
+    }
     
     public function execute() {
 
         // create connection to write DB
         $lb = MediaWikiServices::getInstance()->getDBLoadBalancer();
-
         $dbw = $lb->getConnectionRef( DB_PRIMARY );
 
         // get params and make sure class name is provided
@@ -28,10 +32,7 @@ class ApiCreateClass extends ApiBase {
 
         $vals = [
             'class_name' =>$params['class_name'],
-            'class_start' => '2022-01-01',
-            'class_end' => '2022-01-11',
-            'class_teacher_id' => 1,
-            'class_token' => '213sada2'
+            'class_teacher_user_id' => 5
         ];
 
         // create class row in DB
@@ -44,8 +45,8 @@ class ApiCreateClass extends ApiBase {
         }
 
         $stuff = [$vals];
-        $r = [ 'new_class' => $stuff ];
-		$this->getResult()->addValue( null, $this->getModuleName(), $r );
+        $r = [ 'created_class' => $stuff ];
+        $this->getResult()->addValue( null, $this->getModuleName(), $r );
 
 	}
 
@@ -58,8 +59,11 @@ class ApiCreateClass extends ApiBase {
         ];
     }
 
-    public function getParamDescription() {
-        return array('class_name' => 'The name given to the class of students.');
-    }
+    protected function getExamplesMessages() {
+		return [
+			'action=createclass&class_name=example'
+				=> 'apihelp-query+createclass-example-1'
+		];
+	}
 
 }
