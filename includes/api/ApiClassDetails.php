f5688d4e1f9df5de2cffd4c7ce4c63d89101aba3
soraia653 <soraiaoliveira094@gmail.com>
2022-11-09 21:17:11 +0100

    simplified class details api

--- includes/api/ApiClassDetails.php
+++ includes/api/ApiClassDetails.php
@@ -56,15 +56,15 @@ class ApiClassDetails extends \ApiQueryBase {
 
         foreach($res2 as $row2) {
             $vals2 = [
-            'task_id' => (int)$row2->task_id,
-            'task_name' => $row2->task_name,
-            'task_type' => $row2->task_type,
-            'task_subject' => $row2->task_subject,
-            'task_description' => $row2->task_description,
-            'task_reference_link' => $row2->task_reference_link,
-            'task_criteria' => $row2->task_criteria,
-            'article_name' => $row2->page_name,
-            'creation_date' => $row2->creation_date
+            'task_id' => (int)$row2->task_id
+            , 'task_name' => $row2->task_name
+            // , 'task_type' => $row2->task_type
+            // , 'task_subject' => $row2->task_subject
+            // , 'task_description' => $row2->task_description
+            // , 'task_reference_link' => $row2->task_reference_link
+            // , 'task_criteria' => $row2->task_criteria
+            // , 'article_name' => $row2->page_name
+            // , 'creation_date' => $row2->creation_date
             ];
 
             $all_tasks['task' . strval($i)] = $vals2;
@@ -98,11 +98,12 @@ class ApiClassDetails extends \ApiQueryBase {
             $j++;
 
         }
+        /* */
 
         // QUERY 4 - tasks per students
         $res4 = $dbr->newSelectQueryBuilder()
-        ->select(['student_id', 'u.user_name', 't.task_id', 't.task_name', 't.task_type', 't.task_subject', 't.task_description',
-        't.task_reference_link', 't.task_criteria', 't.page_name', 'task_status', 'task_feedback', 'task_result'])
+        ->select(['student_id', 'u.user_name', 't.task_id', 't.task_name', 'task_status' /*, 't.task_type', 't.task_subject', 't.task_description',
+        't.task_reference_link', 't.task_criteria', 't.page_name',  'task_feedback', 'task_result'*/])
         ->from('wm_tasks_to_students')
         ->join('wm_tasks', 't', 'wm_tasks_to_students.task_id=t.task_id')
         ->join('user', 'u', 'wm_tasks_to_students.student_id=u.user_id')
@@ -118,19 +119,19 @@ class ApiClassDetails extends \ApiQueryBase {
 
         foreach($res4 as $row4) {
             $vals4 = [
-            'student_id' => (int)$row4->student_id,
-            'student_username' => $row4->user_name,
-            'task_id' => (int)$row4->task_id, 
-            'task_name' => $row4->task_name, 
-            'task_type' => $row4->task_type, 
-            'task_subject' => $row4->task_subject, 
-            'task_description' => $row4->task_description,
-            'task_reference_link' => $row4->task_reference_link, 
-            'task_criteria' => $row4->task_criteria, 
-            'article_name' => $row4->page_name, 
-            'task_status' => $row4->task_status, 
-            'task_feedback' => $row4->task_feedback, 
-            'task_result' => $row4->task_result
+            'student_id' => (int)$row4->student_id
+            ,'student_username' => $row4->user_name
+            ,'task_id' => (int)$row4->task_id
+            ,'task_name' => $row4->task_name
+            ,'task_status' => $row4->task_status
+            // ,'task_type' => $row4->task_type
+            // ,'task_subject' => $row4->task_subject
+            // ,'task_description' => $row4->task_description
+            // ,'task_reference_link' => $row4->task_reference_link
+            // ,'task_criteria' => $row4->task_criteria
+            // ,'article_name' => $row4->page_name
+            // ,'task_feedback' => $row4->task_feedback
+            // ,'task_result' => $row4->task_result
             ];
 
             $tasks_per_students['student_task' . strval($k)] = $vals4;
